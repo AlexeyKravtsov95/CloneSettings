3fad80a881b096a8318d50f975b65ef62c8670b4
a.kravcov <a.kravcov@movavi.com>
2021-07-29 03:31:44 +0700

    Add icon

--- CloneSettings/TableViewCell.swift
+++ CloneSettings/TableViewCell.swift
@@ -12,8 +12,10 @@ class TableViewCell: UITableViewCell {
     
     static let identification = "id"
     
-    func configure(with title: String) {
-        titleLabel.text = title
+    func configure(with model: Model) {
+        titleLabel.text = model.title
+        icon.image = model.image
+        containerView.backgroundColor = model.imageBackgroundColor
     }
     
     public lazy var titleLabel: UILabel = {
@@ -23,15 +25,44 @@ class TableViewCell: UITableViewCell {
         return title
     }()
     
+    public lazy var icon: UIImageView = {
+        let icon = UIImageView()
+        icon.contentMode = .scaleAspectFill
+        icon.clipsToBounds = true
+        
+        return icon
+    }()
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
     private func setupHierarchy() {
         addSubview(titleLabel)
+        addSubview(containerView)
+        containerView.addSubview(icon)
     }
     
     private func setupLayout() {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 64).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 52).isActive = true
         titleLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalToConstant: 84).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 84).isActive = true
+        
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        icon.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12).isActive = true
+        icon.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        icon.heightAnchor.constraint(equalToConstant: 32).isActive = true
     }
     
     required init?(coder: NSCoder) {
--- CloneSettings/ViewController.swift
+++ CloneSettings/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate {
     
     let data = [
-        "Section0": [Model(title: "Авиарежим"), Model(title: "Wi-Fi"), Model(title: "Bluetooth"), Model(title: "Сотовая связь"), Model(title: "Режим модема"), Model(title: "VPN")],
-        "Section1": [Model(title: "Уведомления"), Model(title: "Звук, тактильные сигналы"), Model(title: "Не беспокоить"), Model(title: "Экранное время")],
-        "Section2": [Model(title: "Основные"), Model(title: "Пункт управления"), Model(title: "Экран и яркость"), Model(title: "Экран \"Домой\""), Model(title: "Универсальный доступ"), Model(title: "Обои"), Model(title: "Siri и Поиск"), Model(title: "Face ID и код-пароль"), Model(title: "Экстренный вызов - SOS"), Model(title: "Уведомления о контакте"), Model(title: "Аккумулятор"), Model(title: "Конфиденциальность")]
+        "Section0": [Model(title: "Авиарежим",
+                           image: UIImage(systemName: "airplane")),
+                     Model(title: "Wi-Fi",
+                           image: UIImage(systemName: "wifi")),
+                     Model(title: "Bluetooth",
+                           image: UIImage(named: "bluetooth")),
+                     Model(title: "Сотовая связь", image: UIImage(systemName: "antenna.radiowaves.left.and.right")),
+                     Model(title: "Режим модема",
+                           image: UIImage(systemName: "personalhotspot")),
+                     Model(title: "VPN",
+                           image: UIImage(systemName: "globe"))],
+        "Section1": [Model(title: "Уведомления",
+                           image: UIImage(systemName: "note")),
+                     Model(title: "Звук, тактильные сигналы",
+                           image: UIImage(systemName: "speaker.wave.3.fill")),
+                     Model(title: "Не беспокоить",
+                           image: UIImage(systemName: "moon.fill")),
+                     Model(title: "Экранное время",
+                           image: UIImage(systemName: "hourglass"))],
+        "Section2": [Model(title: "Основные",
+                           image: UIImage(systemName: "gear")),
+                     Model(title: "Пункт управления",
+                           image: UIImage(systemName: "slider.horizontal.3")),
+                     Model(title: "Экран и яркость",
+                           image: UIImage(systemName: "textformat.size")),
+                     Model(title: "Экран \"Домой\"",
+                           image: UIImage(systemName: "apps.iphone")),
+                     Model(title: "Универсальный доступ",
+                           image: UIImage(systemName: "figure.stand")),
+                     Model(title: "Обои",
+                           image: UIImage(systemName: "seal")),
+                     Model(title: "Siri и Поиск",
+                           image: UIImage(named: "siri")),
+                     Model(title: "Face ID и код-пароль",
+                           image: UIImage(systemName: "person.fill.viewfinder")),
+                     Model(title: "Аккумулятор",
+                           image: UIImage(systemName: "battery.100")),
+                     Model(title: "Конфиденциальность",
+                           image: UIImage(systemName: "hand.raised.fill"))]
     ]
     
     private lazy var tableView: UITableView = {
@@ -22,6 +58,15 @@ class ViewController: UIViewController, UITableViewDelegate {
         tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.identification)
         
         return tableView
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("test", for: .normal)
+        button.backgroundColor = .white
+        button.setImage(UIImage(named: "bluetooth"), for: .normal)
+        
+        return button
     }()
     
     override func viewDidLoad() {
@@ -57,7 +102,8 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identification, for: indexPath) as? TableViewCell else { return UITableViewCell() }
         cell.titleLabel.text = data["Section\(indexPath.section)"]?[indexPath.row].title
-
+        cell.icon.image = data["Section\(indexPath.section)"]?[indexPath.row].image
+        
         return cell
     }
 }
